e8b1f73376d520374ee60d2e8800e89d834a1b83
M-Joyce <Mark4751@gmail.com>
2021-09-21 10:05:34 -0600

    Fixed index OOB. used equalsingorecase instead of == and tolowercase

--- src/main/java/me/vetovius/regionbattlemod/CommandChat.java
+++ src/main/java/me/vetovius/regionbattlemod/CommandChat.java
@@ -23,36 +23,34 @@ public class CommandChat implements CommandExecutor {
 
         Player p = Bukkit.getPlayer(sender.getName());
 
-        if(args[0].toLowerCase() == "s"){
-            if(battleChatPlayers.contains(p)){ //remove players from other chat, only allow one chat at a time.
-                battleChatPlayers.remove(p);
-            }
-            if(survivalChatPlayers.contains(p)){
-                survivalChatPlayers.remove(p);
-                p.sendMessage("You have been removed from [Survival]");
-            }
-            else{
-                survivalChatPlayers.add(p);
-                p.sendMessage("You are now chatting in [Survival]");
-            }
-        }
-        else if(args[0].toLowerCase() == "b"){
-            if(survivalChatPlayers.contains(p)){ //remove players from other chat, only allow one chat at a time.
-                survivalChatPlayers.remove(p);
-            }
-            if(battleChatPlayers.contains(p)){
-                battleChatPlayers.remove(p);
-                p.sendMessage("You have been removed from [Battle]");
-            }
-            else{
-                battleChatPlayers.add(p);
-                p.sendMessage("You are now chatting in [Battle]");
-            }
-        }
-        else{
-            p.sendMessage("Invalid chat! use /chat then s or b.");
-        }
+        if(args.length > 0) {
 
+            if (args[0].equalsIgnoreCase("s")) {
+                if (battleChatPlayers.contains(p)) { //remove players from other chat, only allow one chat at a time.
+                    battleChatPlayers.remove(p);
+                }
+                if (survivalChatPlayers.contains(p)) {
+                    survivalChatPlayers.remove(p);
+                    p.sendMessage("You have been removed from "+ChatColor.YELLOW+"[Survival]");
+                } else {
+                    survivalChatPlayers.add(p);
+                    p.sendMessage("You are now chatting in "+ChatColor.YELLOW+"[Survival]");
+                }
+            } else if (args[0].equalsIgnoreCase("b")) {
+                if (survivalChatPlayers.contains(p)) { //remove players from other chat, only allow one chat at a time.
+                    survivalChatPlayers.remove(p);
+                }
+                if (battleChatPlayers.contains(p)) {
+                    battleChatPlayers.remove(p);
+                    p.sendMessage("You have been removed from "+ChatColor.RED+"[Battle]");
+                } else {
+                    battleChatPlayers.add(p);
+                    p.sendMessage("You are now chatting in "+ChatColor.RED+"[Battle]");
+                }
+            } else {
+                p.sendMessage("Invalid chat! use /chat then s or b.");
+            }
+        }
 
         return true;
     }
