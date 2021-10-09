392eea41901d73cd111b9078238662f2aa0be6f5
M-Joyce <Mark4751@gmail.com>
2021-10-09 16:07:29 -0600

    Updated LootItem chances

--- src/main/java/me/vetovius/regionbattlemod/LootItem.java
+++ src/main/java/me/vetovius/regionbattlemod/LootItem.java
@@ -9,16 +9,16 @@ import java.util.Random;
 public enum LootItem{
 
     Iron_Ingot(Material.IRON_INGOT,2,4),
-    Cooked_Beef(Material.COOKED_BEEF,7,4),
+    Cooked_Beef(Material.COOKED_BEEF,10,4),
     Golden_Apple(Material.GOLDEN_APPLE,1,2),
     Stone_Axe(Material.STONE_AXE,7,1),
     Stone_Sword(Material.STONE_SWORD,7,1),
     Diamond(Material.DIAMOND,1,1),
-    Oak_Log(Material.OAK_LOG,7,2),
-    Leather_Chestplate(Material.LEATHER_CHESTPLATE,1,1),
-    Leather_Boots(Material.LEATHER_BOOTS,1,1),
-    Leather_Leggings(Material.LEATHER_LEGGINGS,1,1),
-    Leather_Helmet(Material.LEATHER_HELMET,1,1),
+    Oak_Log(Material.OAK_LOG,12,2),
+    Leather_Chestplate(Material.LEATHER_CHESTPLATE,2,1),
+    Leather_Boots(Material.LEATHER_BOOTS,4,1),
+    Leather_Leggings(Material.LEATHER_LEGGINGS,2,1),
+    Leather_Helmet(Material.LEATHER_HELMET,4,1),
     Iron_Sword(Material.IRON_SWORD,1,1),
     TNT(Material.TNT,1,3),
     Lighter(Material.FLINT_AND_STEEL,1,1);
