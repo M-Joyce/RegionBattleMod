631fb2936804f9861edac2d3ec9ed82d5c5b9595
M-Joyce <Mark4751@gmail.com>
2021-11-02 22:06:04 -0600

    Added BossBar for capturing progress, there is a bug i think when capturing -> contested -> capturing again

--- src/main/java/me/vetovius/regionbattle/smpbattleregion/BattleRegion.java
+++ src/main/java/me/vetovius/regionbattle/smpbattleregion/BattleRegion.java
@@ -5,7 +5,11 @@ import me.vetovius.regionbattle.CommandSendTeamChat;
 import me.vetovius.regionbattle.RegionBattle;
 import me.vetovius.regionbattle.persistentbattle.CommandStartPersistentBattle;
 import me.vetovius.regionbattle.persistentbattle.PersistentBattle;
+import me.vetovius.regionbattle.regionbattle.Battle;
 import org.bukkit.*;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
@@ -24,9 +28,15 @@ public class BattleRegion {
     private static final int range = 4000; //how far can the zone be from spawn + or -
     private static final int minDistanceFromSpawn = 550; //can't be closer than this to spawn
     private static final int radius = 10; //how big should the battle region square radius be in blocks?
+    private static final int captureBarRadius = 100;
 
     private long startTime = 0;
     private long duration = 0;
+
+    private long timeToCapture = 300000; //5 minutes = 300000ms
+    private long captureStartTime;
+
+    private BossBar captureProgressBar;
 
     private int particleTaskId = 0;
     private int timerTaskId = 0;
@@ -35,17 +45,22 @@ public class BattleRegion {
     private Location battleRegionCenter;
 
     private ArrayList<Player> playersInZone = new ArrayList<Player>();
+    private ArrayList<Player> captureProgressBarPlayersList = new ArrayList<Player>();
+    private Player capturingPlayer;
 
 
     public BattleRegion(RegionBattle pluginInstance){
         this.pluginInstance = pluginInstance;
         findLocationForBattleRegion(); //find a suitable location for the battle region
         particleTaskId=initBattleRegionParticles();
+        //TODO Maybe spawn a monster(s)/boss?
 
         //1 hour = 3600000 miliseconds
         startTime = System.currentTimeMillis();
         duration =  System.currentTimeMillis() + 3600000; //1 hour from now
 
+        this.captureProgressBar = Bukkit.createBossBar(ChatColor.GRAY+"Battle Region", BarColor.RED, BarStyle.SEGMENTED_10);
+        captureProgressBar.setProgress(1);
         startBattleRegionTimer();
     }
 
@@ -73,6 +88,78 @@ public class BattleRegion {
         checkForPlayers = Bukkit.getScheduler().scheduleSyncRepeatingTask(pluginInstance, new Runnable() {
             public void run() {
                 playersInZone = (ArrayList<Player>) battleRegionCenter.getNearbyPlayers(radius); //updating the playersInZone list if zone is empty or contains players.
+                captureProgressBarPlayersList = (ArrayList<Player>) battleRegionCenter.getNearbyPlayers(captureBarRadius);
+
+                for(Player p : captureProgressBar.getPlayers()){
+                    if(!captureProgressBarPlayersList.contains(p)){
+                        captureProgressBar.removePlayer(p);
+                    }
+                }
+                for(Player p : captureProgressBarPlayersList){
+                    if(!captureProgressBar.getPlayers().contains(p)){
+                        captureProgressBar.addPlayer(p);
+                    }
+                }
+
+                if(playersInZone.isEmpty()){
+                    captureProgressBar.setTitle("Battle Region is Empty!");
+                    captureProgressBar.setProgress(1);
+                    captureProgressBar.setColor(BarColor.RED);
+                    capturingPlayer = null;
+                }
+                else if(playersInZone.size() == 1){
+
+
+                    if(capturingPlayer == null){
+                        captureProgressBar.setTitle(playersInZone.get(0).getName() + " is capturing the Battle Region!");
+                        captureProgressBar.setColor(BarColor.GREEN);
+                        capturingPlayer = playersInZone.get(0);
+                        captureStartTime = System.currentTimeMillis();
+                        timeToCapture += System.currentTimeMillis();
+                    }
+                    else if(playersInZone.get(0) != capturingPlayer){ //New player is in control
+                        capturingPlayer = playersInZone.get(0);
+                    }else if(playersInZone.get(0) == capturingPlayer){
+                        if(System.currentTimeMillis() > captureStartTime + timeToCapture){
+                            //Zone is captured
+                            Bukkit.getScheduler().cancelTask(checkForPlayers);
+
+                            for(Player p : smpWorld.getPlayers()){
+                                p.sendMessage("The battle region has been captured by " + capturingPlayer.getName() +"!");
+                            }
+
+                            //TODO reward capturing player
+                            //TODO maybe give capturing player a strength/armor buff for 1 minute?
+
+                            //after 1 minute cancel all the RegionBattle Stuff.
+                            Bukkit.getScheduler().scheduleSyncDelayedTask(pluginInstance, new Runnable() {
+                                @Override
+                                public void run() {
+                                    Bukkit.getScheduler().cancelTask(timerTaskId);
+                                    Bukkit.getScheduler().cancelTask(particleTaskId);
+                                }
+                            }, 20*60); //1 minute delay
+
+
+                            captureProgressBar.setProgress(0);
+                            captureProgressBar.setTitle("Battle Region has been captured by: " + capturingPlayer.getName());
+
+                        }
+                        else{
+                            double currPerc = (100 - ((System.currentTimeMillis() - captureStartTime) * 100) / (timeToCapture - captureStartTime)) * 0.01;
+                            captureProgressBar.setProgress(currPerc);
+                        }
+
+                    }
+
+                }
+                else if(playersInZone.size() > 1){
+                    captureProgressBar.setTitle("Battle Region is Contested!");
+                    capturingPlayer = null;
+                    captureProgressBar.setColor(BarColor.YELLOW);
+                }
+
+
                 //For Debugging LOGGER.info(playersInZone.toString());
 
             }}, 20, 20*5); //repeat task every 5 seconds, assuming 20TPS.
@@ -100,6 +187,7 @@ public class BattleRegion {
 
         LOGGER.info("battleRegionCenter Found. X: " + x + " Z: " + z);
         battleRegionCenter = new Location(smpWorld,x,smpWorld.getHighestBlockYAt(x,z)+1,z); //set battleRegionCenter
+        //TODO there should be a way to get this announcement every so often so players that join after its creation can find it.
         for(Player p : smpWorld.getPlayers()){
             p.sendMessage("A BattleRegion has appeared at X: " + x + " Z: "+ z +". Control it for a reward!");
         }
